06da27e9bca49021342319c53ea00565d98ed2b2
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-11-20 10:18:42 +0100

    some stuff

---
+++ autoWeb.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+const delay = require('delay');
+
+const url = 'https://cdon.se';
+const arg = process.argv[2];
+
+const randomNum = (min, max) => Math.random() * (max - min) + min;
+
+const humanDelay = async fixedDelay =>
+  fixedDelay ? await delay(fixedDelay) : await delay(randomNum(500, 3000));
+
+const autoWeb = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    width: 900,
+    height: 600
+  });
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const searchSelector = '#q';
+  await page.type(searchSelector, arg);
+
+  const btnSelector = '#header-search > button';
+  await page.click(btnSelector);
+  const itemSelector =
+    '#ext-search > div.search-results > ul > li:nth-child(1)';
+  await humanDelay();
+  await page.click(itemSelector);
+};
+
+autoWeb();
